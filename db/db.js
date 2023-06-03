8646ba479b5d594fbcd87a382b2cc6b6524f95e2
Ganesh Garide <57977717+Ganesh429@users.noreply.github.com>
2023-06-03 19:00:43 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,7 +1,7 @@
 let mongoose = require('mongoose')
 
 mongoose.set('strictQuery', true)
-mongoose.connect('mongodb://127.0.0.1:27017/searchdatabase', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://pactime:0EM1eJ2N8uXWCEts@cluster0.i5xrelh.mongodb.net/owleeloc?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.connection.once('open', function() {
     console.log('database connected')
 }).on('error', function(error) {
